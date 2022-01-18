4c3ca6b3b33f7f340afa60e4a31157c59c21f2c0
Josh Tillinghast <jwtill@icloud.com>
2022-01-18 11:46:45 -0800

    Refactored jscript with loop
    Co-authored-by: Justin Dechow <justin.dechow@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-// $(document).ready(function() {
-//   $("#formOne").submit(function(event) {
-//     event.preventDefault();
+ $(document).ready(function() {
+   $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+
+    const madLibArray = [$("input#person1").val(), $("input#person2").val(), $("input#animal").val(), $("input#exclamation").val(), $("input#verb").val(), $("input#noun").val()];
+    console.log(madLibArray);
+
+    madLibArray.forEach(function (element) {
+      $(".results").append("<li>" + element + "</li>");
+      
+    });
+
+   });
+ });
+ 
+
 //     const person1Input = $("input#person1").val();
 //     const person2Input = $("input#person2").val();
 //     const animalInput= $("input#animal").val();
@@ -19,6 +33,8 @@
 //   });
 // });
 
+
+
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
@@ -28,6 +44,8 @@ $(document).ready(function() {
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
+
+    const madlibArray = []
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
